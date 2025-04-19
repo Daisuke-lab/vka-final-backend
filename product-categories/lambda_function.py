5a0128db09ffa91c0bf40ba7a1567cb12825811c
Daisuke-lab <kikuchidaisuke.zr@gmail.com>
2025-04-19 12:04:13 -0500

    product-categories function added

---
+++ product-categories/lambda_function.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+import simplejson as json
+
+dynamodb = boto3.resource('dynamodb')
+TABLE_NAME = "ProductTable"
+table = dynamodb.Table(TABLE_NAME)
+def lambda_handler(event, context):
+    print("HELLO WORLD")
+    try:
+        scan_kwargs = {
+            "ProjectionExpression": "category",
+        }
+        categories = table.scan(**scan_kwargs)['Items']
+        # TODO implement
+        return {
+            'statusCode': 200,
+            'body': list(set([category['category'] for category in categories]))
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 400,
+            'body': str(e)
+        }
+
