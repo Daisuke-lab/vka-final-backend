3b69888e6f31b81bc80c2f2a9a6f107629d64758
Daisuke-lab <kikuchidaisuke.zr@gmail.com>
2025-04-20 14:18:52 -0500

    pipeline test

--- terraform/products/get_categories.tf
+++ terraform/products/get_categories.tf
@@ -1,10 +1,22 @@
 variable categories_filename {
   default = "get_categories"
 }
+
+resource "null_resource" "hash_categories_file" {
+  provisioner "local-exec" {
+    command = "sha256sum ${dirname(path.cwd)}/products/${var.categories_filename}.py | awk '{print $1}' > hash_${var.categories_filename}.txt"
+  }
+}
+data "local_file" "hash_categories_file" {
+  filename = "hash_${var.categories_filename}.txt"
+  depends_on = [null_resource.hash_categories_file]
+}
+
 data "archive_file" "init_get_categories" {
   type        = "zip"
   source_file = "${dirname(path.cwd)}/products/${var.categories_filename}.py"
   output_path = "${dirname(path.cwd)}/outputs/${var.categories_filename}.zip"
+
 }
 
 resource "aws_lambda_function" "get_categories" {
@@ -12,6 +24,6 @@ resource "aws_lambda_function" "get_categories" {
   handler          = "${var.categories_filename}.lambda_handler"
   runtime          = "python3.10"
   filename         = data.archive_file.init_get_categories.output_path
-  source_code_hash = base64sha256(file(data.archive_file.init_get_categories.output_path))
+  source_code_hash = data.local_file.hash_categories_file.content_base64
   role             = var.lambda_iam_role
 }
